8004451124df7e7a26435bdfd15ddc3646e18e0f
Gigakoks1337 <162459201+Gigakoks1337@users.noreply.github.com>
2024-03-20 13:14:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class VerticalPrint {
+    public static void main(String[] args) {
+        String text = "Hello, World!";
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println(text.charAt(i));
+        }
+    
+}
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
